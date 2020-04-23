c92b339d424efbe1f3056e620a01fd1bfbc72e7a
luoqiang <luoqiang@biteanbox.com>
2020-04-23 20:51:38 +0800

    修改

--- hello.c
+++ hello.c
@@ -5,3 +5,14 @@ int main(int argc, char *argv[])
     printf("helloWorld!\n");
     printf("\n");
 }
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
